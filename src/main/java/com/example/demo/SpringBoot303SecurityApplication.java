f710879f0fdda32282b67ba6d0f9a2c2c83cad4b
jisookwon <jinskwon@hotmail.com>
2019-05-28 17:41:20 -0400

    still working

---
+++ src/main/java/com/example/demo/SpringBoot303SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot303SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot303SecurityApplication.class, args);
+	}
+
+}
